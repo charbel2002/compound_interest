5a7ffae5bb937128bcb49a4e14510efdf7df610f
charbel2002 <55005228+charbel2002@users.noreply.github.com>
2022-03-23 19:08:21 +0100

    Add files via upload

---
+++ Astra/Astra/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+
+    Dim Uo, Tx, n, val As Double
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RichTextBox1.Text = "Hello i am Astra, i'm gonna compute a total valorisation of an investment based on certain timeframe and interest rate. Even though, make sure to understand that this calculation is just a digit, some economical factors can reduce a little bit this valorisation, anyway this an approximative value of how much you can get at a fix inerest rate and an antifragil strategy in your investment"
+
+        RichTextBox1.Enabled = False
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+
+            Uo = Convert.ToDouble(TextBox1.Text)
+            Tx = Convert.ToDouble(TextBox2.Text)
+            n = Convert.ToDouble(TextBox3.Text)
+
+            val = Uo * Math.Pow((1 + Tx / 100), n)
+
+            TextBox4.Text = val.ToString("##,##,###.00")
+
+        Catch ex As Exception
+
+            MsgBox("Please, fill accurate data type in")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+End Class
---
+++ Astra/Astra/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Astra")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Astra")> 
+<Assembly: AssemblyCopyright("Copyright ©  2022")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bfba58f1-0104-4df8-96ea-c309f37be3f4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
